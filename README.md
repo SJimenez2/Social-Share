d7cd76d69f6f70757b79666216ed196ac9faf909
Sam Jimenez <sjimenez2@elon.edu>
2018-09-24 12:06:57 -0400

    Back button added to Scan QR Code screen

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Social-Share
+
+
+- Line 21 can be uncommented to make the app begin from the initial screen each time it is opened
--- Social Share/QRCodeReader.swift
+++ Social Share/QRCodeReader.swift
@@ -10,10 +10,12 @@
 import UIKit
 import AVFoundation
 
-class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class QRViewController: UIViewController {
     
+    @IBOutlet weak var topBar: UIView!
     var video = AVCaptureVideoPreviewLayer()
     var theirInfo: String = ""
+    var qrCodeFrameView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,31 +25,61 @@ class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
         let session = AVCaptureSession()
         
         //Define capture devcie
-        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        let deviceDescovery = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
+        let captureDevice = deviceDescovery.devices.first
         
         do
         {
             let input = try AVCaptureDeviceInput(device: captureDevice!)
             session.addInput(input)
+            
+            let output = AVCaptureMetadataOutput()
+            session.addOutput(output)
+            
+            output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            
+            output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
         }
         catch
         {
             print ("ERROR")
         }
         
-        let output = AVCaptureMetadataOutput()
-        session.addOutput(output)
-        
-        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        
-        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
         
         video = AVCaptureVideoPreviewLayer(session: session)
+        video.videoGravity = AVLayerVideoGravity.resizeAspectFill
         video.frame = view.layer.bounds
         view.layer.addSublayer(video)
         
+        
         session.startRunning()
+        
+        view.bringSubview(toFront: topBar)
+        
+        qrCodeFrameView = UIView()
+        
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toAddAcounts" {
+            let vc = segue.destination as! AddAccounts
+            vc.saveRecievedInfo(info: theirInfo)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension QRViewController: AVCaptureMetadataOutputObjectsDelegate {
     
     func metadataOutput(_ captureOutput: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         
@@ -63,15 +95,5 @@ class QRViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate
                 }
             }
         }
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let vc = segue.destination as! AddAccounts
-        vc.saveRecievedInfo(info: theirInfo)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
